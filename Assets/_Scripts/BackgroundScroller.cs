55adf16962a2b78771cbbf19863331c8f35f949d
soufyanhassan <18358@ma-web.nl>
2016-09-21 14:40:23 +0200

    first commit

---
+++ Assets/_Scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField]private float _speed;
+    private MeshRenderer _renderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this._renderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 offset = new Vector2(Time.time * _speed, 0);
+        _renderer.material.mainTextureOffset = offset;
+	}
+}
---
+++ Assets/_Scripts/SpriteBGScroller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteBGScroller : MonoBehaviour
+{
+    [SerializeField]private float _speed;
+    private Vector3 _newPos = new Vector3(60, 0, 0);
+    //[SerializeField]private float tileSizeX;
+
+    private Vector3 _startPos;
+
+	void Start ()
+    {
+        _startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //float _newPos = Mathf.Repeat(Time.time * _speed, tileSizeX);
+        //transform.position = _startPos + Vector3.left * _newPos;
+        transform.Translate(Vector3.left * _speed * Time.deltaTime);
+	}
+
+    void OnBecameInvisible()
+    {
+        transform.position += _newPos;
+    }
+}
